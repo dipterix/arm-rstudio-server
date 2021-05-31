fe9e7aa883a2040c16b7b69f770f367da660724d
Dipterix Wang <8163576+dipterix@users.noreply.github.com>
2021-05-31 13:05:33 -0400

    Update R4.sh

--- R4.sh
+++ R4.sh
@@ -4,7 +4,7 @@ RVER="R-4.1.0"
 
 # In other linux, you might need X11 support, but in raspberry, this is not required
 sudo apt-get install libbz2-dev build-essential libpcre2-dev \
-  fort77 gfortran gcc gobjc++ openjdk-11-jre-headless \
+  fort77 gfortran gcc gobjc++ openjdk-11-jdk openjdk-11-jre-headless \
   libssl-dev libssh2-1-dev libv8-dev libxml2-dev libfftw3-dev \
   libtiff5-dev libhdf5-dev libcurl4-openssl-dev xorg-dev \
   libtiff-dev libcairo2-dev texlive texlive-fonts-extra texinfo
