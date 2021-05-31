fc5a12ae9c6465d6e571b4b392c8cffbbe3ab641
Dipterix Wang <8163576+dipterix@users.noreply.github.com>
2021-05-31 13:08:59 -0400

    Update R4.sh

--- R4.sh
+++ R4.sh
@@ -7,7 +7,8 @@ sudo apt-get install libbz2-dev build-essential libpcre2-dev \
   fort77 gfortran gcc gobjc++ openjdk-11-jdk openjdk-11-jre-headless \
   libssl-dev libssh2-1-dev libv8-dev libxml2-dev libfftw3-dev \
   libtiff5-dev libhdf5-dev libcurl4-openssl-dev xorg-dev \
-  libtiff-dev libcairo2-dev texlive texlive-fonts-extra texinfo
+  libtiff-dev libcairo2-dev texlive texlive-fonts-extra texinfo \
+  libblas-dev liblapack-dev liblzma-dev libreadline-dev
 
 
 mkdir /tmp/
